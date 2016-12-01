4b38272cd48e05dc880dcb0357226756368fe7dd
Rexbanana1 <b30781@student.ghchs.com>
2016-11-30 16:29:51 -0800

    added Lift class and h file

---
+++ src/Lift.h
@@ -0,0 +1,19 @@
+/*
+ * Lift.h
+ *
+ *  Created on: Nov 30, 2016
+ *      Author: Admin
+ */
+#include "wpilib.h"
+#ifndef SRC_LIFT_H_
+#define SRC_LIFT_H_
+
+class Lift
+{
+public:
+private:
+};
+
+
+
+#endif /* SRC_LIFT_H_ */
